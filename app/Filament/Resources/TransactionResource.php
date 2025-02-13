25a49194f9056eb8d923c8a55a10799b45ed96a9
zaenalrfn <ZAOfficial24@gmail.com>
2025-02-13 17:26:26 +0700

    Transactions Resource

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\TransactionResource\Pages;
+use App\Filament\Resources\TransactionResource\Pages\ListTransactions;
 use App\Filament\Resources\TransactionResource\RelationManagers;
 use App\Models\Transaction;
 use Filament\Forms;
@@ -12,6 +13,8 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\TransactionItemsResource\Pages\ListTransactionItems;
+use Filament\Tables\Actions\Action;
 
 class TransactionResource extends Resource
 {
@@ -65,51 +68,67 @@ class TransactionResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('code')
+                    ->label('Transaction Code')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('name')
+                    ->label('Customer Name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('phone')
+                    ->label('Phone Number')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('barcodes.image')
+                    ->label('Barcode'),
+                Tables\Columns\TextColumn::make('payment_method')
+                    ->label('Payment Method')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('payment_status')
+                    ->label('Payment Status')
+                    ->badge()
+                    ->colors([
+                        'success' => fn($state): bool => in_array($state, ['SUCCESS', 'PAID', 'SETTLED']),
+                        'warning' => fn($state): bool => $state === 'PENDING',
+                        'danger' => fn($state): bool => in_array($state, ['FAILED', 'EXPIRED']),
+                    ])
                     ->searchable(),
                 Tables\Columns\TextColumn::make('external_id')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('checkout_link')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('barcodes_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('payment_method')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('payment_status')
-                    ->searchable(),
                 Tables\Columns\TextColumn::make('subtotal')
+                    ->label('Subtotal')
                     ->numeric()
-                    ->sortable(),
+                    ->money('IDR'),
                 Tables\Columns\TextColumn::make('ppn')
+                    ->label('PPN')
                     ->numeric()
-                    ->sortable(),
+                    ->money('IDR'),
                 Tables\Columns\TextColumn::make('total')
+                    ->label('Total')
                     ->numeric()
-                    ->sortable(),
+                    ->money('IDR'),
                 Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Updated At')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
-            ->filters([
-                //
-            ])
+            ->filters([])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Action::make('See transaction')
+                    ->colors('success')
+                    ->url(
+                        fn(Transaction $record): string => static::getUrl('transaction-items.index', [
+                            'parent' => $record->id,
+                        ])
+                    ),
             ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+            ->bulkActions([]);
     }
 
     public static function getRelations(): array
@@ -125,6 +144,7 @@ class TransactionResource extends Resource
             'index' => Pages\ListTransactions::route('/'),
             'create' => Pages\CreateTransaction::route('/create'),
             'edit' => Pages\EditTransaction::route('/{record}/edit'),
+            'transaction-items.index' => ListTransactionItems::route('/{parent}/transaction-items'),
         ];
     }
 }
